932fa1e701b0aec79f7625dcbd6f6264d5463766
zahid <zahidthoker5@gmail.com>
2025-02-19 11:14:14 +0530

    completed the css part and added the function of add button

--- script.js
+++ script.js
@@ -3,10 +3,15 @@ const inputValue = document.getElementById('text-input-box');
 const addButton = document.getElementById('add-btn');
 
 const taskList = document.getElementById('task-list');
-
+let listNumer = 0;
 addButton.addEventListener('click', () => {
+   if(inputValue.value === ""){
+      addButton.disabled = true;}
+ else{
+   listNumer++;
    const task_list =  document.createElement("div");
-   task_list.className = "list";
+   task_list.className = `list`;
+   task_list.id = `list${listNumer}`;
    
   
    const listItem = document.createElement('li');
@@ -14,9 +19,11 @@ addButton.addEventListener('click', () => {
 
    const trash = document.createElement('i');
    trash.className = "fa-sharp fa-solid fa-trash"
+   trash.id = `trash${listNumer}`;
 
    const checkBox = document.createElement('input');
    checkBox.type = "checkbox";
+   checkBox.id = `check${listNumer}`;
 
 
    listItem.appendChild(checkBox);
@@ -29,4 +36,14 @@ addButton.addEventListener('click', () => {
 
    inputValue.value = "";
 //    console.log(task_list);
+ }
+
 });
+
+const trash = document.getElementById(`trash${listNumer}`);
+console.log(trash);
+const listitem = document.getElementById(`list${listNumer}`);
+// trash.addEventListener('click', () => {
+//    taskList.removeChild(listitem);
+//    listNumer--;
+// });
